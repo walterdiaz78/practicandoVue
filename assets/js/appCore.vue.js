6f783920f647f98112b25be6184c6eb5777b1b90
walterdiaz78 <walterdiaz78@gmail.com>
2023-10-16 16:14:20 -0300

    no sale userDB.json en index

---
+++ assets/js/appCore.vue.js
@@ -0,0 +1,15 @@
+import users from "./userDB.json" assert {type: "json" };
+
+
+// instancio el core del argumento
+
+export const appCore = {
+  data: function () {
+    return {
+      count: 0,
+      msg: "La llama que llama!",
+      display: true,
+      users: users,
+    };
+  },
+};
